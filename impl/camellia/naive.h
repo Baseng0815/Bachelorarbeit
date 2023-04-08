32fd73573bbefd7014bf8e69dbf906e30d14776d
Bastian Engel <bastian.engel00@gmail.com>
2023-04-08 06:10:26 +0200

    camellia naive implementation

--- impl/camellia/naive.h
+++ impl/camellia/naive.h
@@ -11,16 +11,22 @@ struct camellia_keytable {
         uint64_t kl[4]; // used in FL layer
 };
 
-uint64_t camellia_naive_S(uint64_t X, uint64_t k);
-uint64_t camellia_naive_P(uint64_t X, uint64_t k);
+uint64_t camellia_naive_S(uint64_t X);
+uint64_t camellia_naive_P(uint64_t X);
 uint64_t camellia_naive_F(uint64_t X, uint64_t k);
 uint64_t camellia_naive_FL(uint64_t X, uint64_t kl);
 uint64_t camellia_naive_FL_inv(uint64_t X, uint64_t kl);
 void camellia_naive_feistel_round(uint64_t state[2], uint64_t kr);
+void camellia_naive_feistel_round_inv(uint64_t state[2], uint64_t kr);
 void camellia_naive_generate_round_keys(const uint64_t key[restrict 2],
                                         struct camellia_keytable *restrict rks);
 
-void camellia_naive_encrypt(uint64_t m[restrict 2],
-                            uint64_t key[restrict 2]);
+void camellia_naive_encrypt(uint64_t c[restrict 2],
+                            const uint64_t m[restrict 2],
+                            struct camellia_keytable *restrict rks);
+
+void camellia_naive_decrypt(uint64_t m[restrict 2],
+                            const uint64_t c[restrict 2],
+                            struct camellia_keytable *restrict rks);
 
 #endif
