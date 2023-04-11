46fea32f8aca765acda7439c5e6556ca27bba244
Baseng0815 <1shedex2@gmail.com>
2023-04-11 13:41:39 +0200

    camellia intro FL

--- scripts/64_vec_sliced_pack.py
+++ scripts/64_vec_sliced_pack.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+p = list()
+
+for i in range(16):
+    src = (i % 2) * 16 + i // 2
+    p.append(src)
+
 print('0x', end='')
 for i in range(15, -1, -1):
-    src = (i % 2) * 16 + i // 2 + 8
-    print(f'{src:02x}', end='')
+    print(f'{p[i]:02x}', end='')
+print('')
+
+print(p)
+
+print('0x', end='')
+for i in range(31, 15, -1):
+    for isrc, src in enumerate(p):
+        if src == i or (src > 7 and src - 8 == i):
+            print(f'{isrc:02x}', end='')
 
 print('')
